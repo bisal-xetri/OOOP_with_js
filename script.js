e8dcfb60df21701d9c60fc161dbd4cf132541b26
bisal-xetri <bishaldkl56@gmail.com>
2024-08-16 21:11:59 +0545

    Prototype inheritance

--- script.js
+++ script.js
@@ -42,4 +42,24 @@ console.log(Person.prototype.isPrototypeOf(Person))
 //.prototypeOfLinkedObjects
 
 Person.prototype.species='Homo sapiens';
-console.log(bishal.species)
+console.log(bishal.species);
+
+console.log(bishal.__proto__);
+console.log(bishal.__proto__.__proto__);
+console.log(bishal.__proto__.__proto__.__proto__);
+
+console.log(Person.prototype.constructor);
+
+const arr=[3,3,6,9,3,10];
+console.log(arr.__proto__===Array.prototype);
+
+console.log(arr.__proto__.__proto__);
+
+
+Array.prototype.unique=function (){
+    return [...new Set(this)];
+}
+console.log(arr.unique());
+
+const h1=document.querySelector("h1");
+console.dir(x=>x+1);
