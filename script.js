5cbb252579b07cb637e48f8ec440cdafc321d1d4
bisal-xetri <bishaldkl56@gmail.com>
2024-08-17 21:27:26 +0545

    challenge 3

--- script.js
+++ script.js
@@ -192,7 +192,7 @@ anjila.calAge();
 
 //challenge 2
 
-
+/*
 class CarCl{
     constructor(make, speed){
     this.make=make;
@@ -265,3 +265,44 @@ biki.introduce();
 
 
  console.dir(Student.prototype.constructor)
+ */
+ //challenge 3
+  const Car= function(make, speed){
+        this.make=make;
+        this.speed=speed;
+    }
+    
+    Car.prototype.accelerate=function(){
+    
+        this.speed+=10;
+        console.log(`${this.make} is going at ${this.speed} km/h`);
+    }
+    Car.prototype.break=function(){
+    
+        this.speed-=5;
+        console.log(`${this.make} is going at ${this.speed} km/h`);
+    }
+    
+    const EV= function(make, speed, charge){
+        Car.call(this, make, speed)
+       
+        this.charge=charge;
+    }
+    EV.prototype=Object.create(Car.prototype);
+    EV.prototype.chargeBattery= function(chargeTo){
+        this.charge=chargeTo;
+    } 
+
+     EV.prototype.accelerate=function(){
+        this.speed+=20;
+        this.charge--;
+        console.log(`${this.make} is going at ${this.speed} km/h, with a charge of ${this.charge}`);
+    
+     }
+    const tesla=new EV("Tesla",120,23)
+
+    tesla.chargeBattery(90)
+    console.log(tesla)
+
+    tesla.break();
+    tesla.accelerate()
